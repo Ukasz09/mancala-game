1111a8b49eb9c4133e9192262da5c5f54801cb7d
ukasz09 <246703@student.pwr.edu.pl>
2021-05-02 12:51:34 +0200

    Add bg and hole/board switch

--- src/app/pages/game/board/board.component.ts
+++ src/app/pages/game/board/board.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  private boardNumber = 0;
+  private maxBoardNumber = 7;
+  private holeNumber = 0;
+  private maxHoleNumber = 9;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public onNextBoardClick(): void {
+    this.boardNumber = ++this.boardNumber % (this.maxBoardNumber + 1);
   }
 
+  public onNextHoleClick(): void {
+    this.holeNumber = ++this.holeNumber % (this.maxHoleNumber + 1);
+  }
+
+  /* ------------------------------------------- Getters / setters ------------------------------------------- */
+  get boardImageUrl(): string {
+    return `url(/assets/images/cartoon-wood-00${this.boardNumber}.jpg)`;
+  }
 }
