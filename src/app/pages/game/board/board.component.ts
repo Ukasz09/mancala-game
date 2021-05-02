6c27147ed6292038ac57324d332cd69c1c25038d
ukasz09 <246703@student.pwr.edu.pl>
2021-05-02 13:52:28 +0200

    Add holes; Change board design; Remove some of the boards

--- src/app/pages/game/board/board.component.ts
+++ src/app/pages/game/board/board.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-board',
@@ -7,13 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   private boardNumber = 0;
-  private maxBoardNumber = 7;
+  private maxBoardNumber = 4;
   private holeNumber = 0;
-  private maxHoleNumber = 9;
+  private maxHoleNumber = 1;
+  private holesQtyInRow = 6;
+
+  holeNumbers: number[];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initHoleNumbers();
+  }
+
+  private initHoleNumbers(): void {
+    this.holeNumbers = Array(this.holesQtyInRow)
+      .fill(1)
+      .map((_x, i) => i + 1);
+  }
 
   public onNextBoardClick(): void {
     this.boardNumber = ++this.boardNumber % (this.maxBoardNumber + 1);
@@ -24,6 +41,10 @@ export class BoardComponent implements OnInit {
   }
 
   /* ------------------------------------------- Getters / setters ------------------------------------------- */
+  get holeImageUrl(): string {
+    return `url(/assets/images/tree-hollow-00${this.holeNumber}.png)`;
+  }
+
   get boardImageUrl(): string {
     return `url(/assets/images/cartoon-wood-00${this.boardNumber}.jpg)`;
   }
--- src/app/pages/game/board/components/hole/hole.component.ts
+++ src/app/pages/game/board/components/hole/hole.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hole',
   templateUrl: './hole.component.html',
-  styleUrls: ['./hole.component.scss']
+  styleUrls: ['./hole.component.scss'],
 })
 export class HoleComponent implements OnInit {
+  @Input() size: number;
+  @Input() holeImageUrl: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
